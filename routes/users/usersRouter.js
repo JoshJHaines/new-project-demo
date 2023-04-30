bd10c025b6b30d42c54758e49fd84f917a2db9ee
JoshJHaines <joshjhaines3@gmail.com>
2023-04-30 19:39:54 -0400

    making routes function

--- routes/users/usersRouter.js
+++ routes/users/usersRouter.js
@@ -12,28 +12,28 @@ router.get('/', function(req, res, next) {
   });
 });
 
-// router.get('/get-all-users', function(req, res, next) {
-//   res.json({
-//     status:"success",
-//     message: "You have gotten all users." 
-//   });
-// });
-
-router.get('/get-all-users', function(req, res){
-  userController.getAllUsers(function (err, payload){
-    if (err){
-      res.status(500).json({
-        message: "Error",
-        error: "shucks " + err
-      });
-    } else {
-      res.json({
-        status: "Success",
-        message: "You did it!",
-        data: payload
-      });
-    }
+router.get('/get-all-users', function(req, res, next) {
+  res.json({
+    status:"success",
+    message: "You have gotten all users." 
   });
 });
+
+// router.get('/get-all-users', function(req, res){
+//   userController.getAllUsers(function (err, payload){
+//     if (err){
+//       res.status(500).json({
+//         message: "Error",
+//         error: "shucks " + err
+//       });
+//     } else {
+//       res.json({
+//         status: "Success",
+//         message: "You did it!",
+//         data: payload
+//       });
+//     }
+//   });
+// });
 
 module.exports = router;
